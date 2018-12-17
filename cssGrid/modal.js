d1093bdbc718b6884830bdd1198f8db7bad423a7
dppowell <33650856+dppowell@users.noreply.github.com>
2018-12-17 13:45:44 -0500

    Add files via upload

--- cssGrid/modal.js
+++ cssGrid/modal.js
@@ -1,4 +1,73 @@
+function changeTextColor(colorSelect){
+		localStorage.setItem("textColor", colorSelect);
+
+		var text = document.querySelectorAll(".changeColorText");
+		console.log(text);
+
+		for (var i = 0; i < text.length; i++)
+		{		
+			text[i].style.color =  colorSelect;
+		}
+		
+		
+		
+		var svgs = document.querySelectorAll(".st0");
+		
+		for (var i = 0; i < svgs.length; i++)
+			 {
+				svgs[i].style.stroke=colorSelect;
+			}
+}
+
+
+function changeBackgroundColor(backSelect){
+	
+	var backgrounds = document.querySelectorAll(".background");
+	localStorage.setItem("backColor", backSelect);
+	for (var i = 0; i < backgrounds.length; i++)
+		 {
+
+			backgrounds[i].style.background =  backSelect;
+
+		}	
+	
+}
+
+function changeFont(font){
+	localStorage.setItem("fontFamily", font);
+	var text = document.querySelectorAll(".changeColorText");
+		for (var i = 0; i < text.length; i++)
+			{
+				text[i].style.fontFamily=font.value;
+			}
+}
+
+
+function presetLoad(){
+	
+	var presetFont = localStorage.getItem("fontFamily");
+		if (presetFont !== null){
+				changeFont(presetFont);
+			}
+		
+	var presetBackground = localStorage.getItem("backColor");
+	console.log(presetBackground)
+		if (presetBackground !== null){
+				changeBackgroundColor(presetBackground);
+			}
+		
+	var presetTextColor = localStorage.getItem("textColor");
+	
+		if (presetTextColor !== null){
+				changeTextColor(presetTextColor);
+			}
+		
+}
+
+
 document.addEventListener ("DOMContentLoaded",function() {
+	
+	presetLoad();
 	// JavaScript Document
 	// Get the modal
 	var modal = document.getElementById('myModal');
@@ -28,21 +97,47 @@ document.addEventListener ("DOMContentLoaded",function() {
 	
 	var submitButton = document.getElementById("btnTwo");
 	
-	submitButton.onclick = function()  { 
+	submitButton.onclick = function()  {
+		
 		var colorSelect = document.getElementById("textColor").value;
-		var text = document.querySelectorAll("p");
-		console.log(text);
+		changeTextColor (colorSelect);
+		
 
-		for (var i = 0; i < text.length; i++)
-		{		
-			text[i].style.color =  colorSelect;
-		}
+		
+		var font = document.querySelector(".fontSelector").value;
+		console.log(font);
+		changeFont(font);
+		
+
+		
+		
+		
+//		var colorSelect = document.getElementById("textColor").value;
+//		var svg = document.querySelectorAll(".st0");
+//		console.log (svg);
+//		
+//		
+//		for (var i = 0; i < svg.length; i++)
+//		{		
+//			svg[i].style.color =  colorSelect;
+//		}
+		
+//		function svgMod(colorSelect)
+//			{
+//				var s = document.getElementsByClassName("svgClass");
+//				console.log(s);
+//				for (var i = 0; i<s.length; i++)
+//					{
+//						s[i].setAttribute("stroke",colorSelect);
+//					}
+//			}
+//		svgMod(colorSelect);
 		
 		var backSelect = document.getElementById("backColor").value;
-		var body = document.querySelector("body");
-		body.style.background =  backSelect;
+		changeBackgroundColor(backSelect)
 		modal.style.display = "none";
 	};
+	
 
 	
 	
